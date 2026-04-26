c6e46723fc113c78c902a4a11aadba39d624e2e0
TsotneIvanashvili <Tsotneivanashvili@gmail.com>
2026-04-27 02:35:21 +0400

    main

--- frontend/js/media-config.js
+++ frontend/js/media-config.js
@@ -1,4 +1,4 @@
 window.MEDIA_SOURCES = {
   heroVideo: "8081347-hd_1920_1080_30fps (1).mp4",
-  scrollVideo: "scroll-video.mp4"
+  scrollVideo: "https://res.cloudinary.com/drywd0au5/video/upload/v1777242862/Sony_ZV-E10_II_Cinematic_Video_-_Jayson_Robertson_1080p_h264_v5stzb.mp4"
 };
